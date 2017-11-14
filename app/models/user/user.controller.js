7c41122268de2dac2384100984bf839af87c022c
Quoc Bao Nguyen <punkupoz@gmail.com>
2017-11-14 13:08:53 +1000

    finish basic

--- app/models/user/user.controller.js
+++ app/models/user/user.controller.js
@@ -3,12 +3,8 @@ var randomstring = require('randomstring');
 var bcrypt = require('bcrypt');
 var Promise = require("bluebird");
 
-
-
 exports.create_pending_user = function(req, res, next) {
-
 	var verifyKey = randomstring.generate(30);
-
 	const query = 'SELECT email FROM "user" WHERE email = $1';
 	db.get().query(query, [req.body.email])
 	.then(result => {
@@ -37,56 +33,60 @@ exports.create_pending_user = function(req, res, next) {
 }
 
 exports.create_user = function (req, res, next) {
-	bcrypt.hash(req.body.password, 8)
-	.then((hash)=>{
-		db.get().query(queryGetPending, valuesGetPending)
-		.then(result => {
-			if(result.rowCount === 0) {
-				throw new Error('key or email incorrect');
+	var hash;
+	function havePassword(pass){
+		return new Promise((resolve, reject) => {
+			if(!pass) {
+				throw new Error('no password provided');
+			} else {
+				resolve('ok');
 			}
-			else {
-				const queryInsertUser = 'INSERT INTO "user" (email, password, first_name, last_name) VALUES ($1, $2, $3, $4) RETURNING *';
-				const valuesInsertUser = [result.rows[0].email, hash, result.rows[0].first_name, result.rows[0].last_name];
-				db.get().query(queryInsertUser, valuesInsertUser)
-				.then(result2 => {
-					res.send({
-						ok: true,
-						data:result2
-					});
-				})
-				.catch(e => {
-					res.send({
-						ok:false,
-						message: 'error when inserting new user',
-						error: e.message
-					})
-				})
-			}
-		})
-		.then(() => {
+		});
+	}
+
+	havePassword(req.body.password)
+	.then(() => {
+		return bcrypt.hash(req.body.password, 8)
+	})
+	.then(hashed => {
+		hash = hashed;
+	})
+	.then(() => {
+		const queryGetPending = 'SELECT * FROM "pending_user" WHERE email = $1';
+		const valuesGetPending = [req.query.email, req.query.key];
+		return db.get().any(queryGetPending, valuesGetPending)
+	})
+	.then((result) => {
+		if(result.length === 0) {
+			throw new Error('key or email incorrect');
+		}
+		else{
+			return result;
+		}
+	})
+	.then(result => {
+		return db.get().tx(t => {
+			const queryInsertUser = 'INSERT INTO "user" (email, password, first_name, last_name) VALUES ($1, $2, $3, $4) RETURNING *';
+			const valuesInsertUser = [result[0].email, hash, result[0].first_name, result[0].last_name];
+			const insertUser = db.get().one(queryInsertUser, valuesInsertUser);
 			const queryDeletePending = 'DELETE FROM pending_user WHERE email = $1';
-			db.get().query(queryDeletePending, [req.query.email])
-			.catch(e => {
-				console.log(e);
-			});
+			const deletePendingUser = db.get().none(queryDeletePending, [req.query.email]);
+			return t.batch([insertUser, deletePendingUser]);
 		})
-		.catch(e => {
-			res.send({
-				ok:false,
-				error: e.message
-			})
+	})
+	.then(result => {
+		res.send({
+			ok: true,
+			message: 'User ' + result[0].email + ' has been verified'
 		})
 	})
 	.catch(e => {
+		console.log(e);
 		res.send({
 			ok: false,
-			error: e.message
+			message: e.message
 		});
 	});
-
-	const queryGetPending = 'SELECT * FROM "pending_user" WHERE email = $1 AND verify_key = $2';
-	const valuesGetPending = [req.query.email, req.query.key];
-	
 }
 
 exports.login = function(req, res, next) {
